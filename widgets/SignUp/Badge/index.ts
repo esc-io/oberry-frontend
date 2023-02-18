1062ad69e4b53c4e4e0d33aea916d29b4e166cc2
Woosung Lim <wjdxor1224@gmail.com>
2023-02-18 15:57:44 +0900

    feat: 인증 심사 요청 화면 제작
    인증 심사 요청 화면을 제작하여 반영함.
    
    refs #19

--- widgets/SignUp/Badge/index.ts
+++ widgets/SignUp/Badge/index.ts
@@ -1,3 +1,4 @@
 export { default as BadgeIntroduction } from "./BadgeIntroduction/BadgeIntroduction";
 export { default as BadgeSelect } from "./BadgeSelect/BadgeSelect";
 export { default as CertificationDataSubmit } from "./CertificationDataSubmit/CertificationDataSubmit";
+export { default as CertificationRequest } from "./CertificationRequest/CertificationRequest";
