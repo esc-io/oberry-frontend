6724c5bb1656ce92b772f292a453021438a23841
Woosung Lim <wjdxor1224@gmail.com>
2023-02-24 00:35:50 +0900

    feat: 네비게이션 바를 추가
    하단에 네비게이션 바를 추가하여 반영함.
    
    refs #28

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import { ThemeProvider } from "react-native-magnus";
 import { NavigationContainer } from "@react-navigation/native";
+
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { Provider as PaperProvider } from "react-native-paper";
 import { theme } from "styles/theme";
 
-import Login from "./screens/Login";
-import SignUp from "./screens/SignUp";
+// import Login from "./screens/Login";
+// import SignUp from "./screens/SignUp";
+import Account from "./screens/Account";
+import { BottomNavigation } from "./components";
 
 const RootStack = createNativeStackNavigator();
 
@@ -16,8 +19,9 @@ export default function App() {
       <ThemeProvider>
         <NavigationContainer>
           <RootStack.Navigator screenOptions={{ headerShown: false }}>
-            <RootStack.Screen name="login" component={Login} />
-            <RootStack.Screen name="sign-up" component={SignUp} />
+            {/* <RootStack.Screen name="login" component={Login} /> */}
+            {/* <RootStack.Screen name="sign-up" component={SignUp} /> */}
+            <RootStack.Screen name="account" component={Account} />
           </RootStack.Navigator>
         </NavigationContainer>
       </ThemeProvider>
