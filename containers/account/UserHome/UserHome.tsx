32e484ad2354a07761b162e9d01044a32155b26c
Woosung Lim <wjdxor1224@gmail.com>
2023-02-25 23:46:30 +0900

    feat: 사용자 계정 홈 화면 제작
    사용자 계정 홈 화면을 제작하여 반영함.
    
    refs #30

--- containers/account/UserHome/UserHome.tsx
+++ containers/account/UserHome/UserHome.tsx
@@ -1,18 +1,122 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 
-import { Text } from "react-native-paper";
+import { Text, Avatar, List } from "react-native-paper";
 
 import { AppLayout } from "layouts";
+import { theme } from "styles/theme";
 
 function UserHome() {
+  const menuList = [
+    {
+      title: "회원 만남 후기",
+      navigate: "",
+    },
+    {
+      title: "포인트 충전",
+      navigate: "",
+    },
+    {
+      title: "선호 이성 설정",
+      navigate: "",
+    },
+    {
+      title: "지인 만남 차단",
+      navigate: "",
+    },
+  ];
+
+  const settings = [
+    {
+      title: "비밀번호 변경",
+      navigate: "",
+    },
+    {
+      title: "앱 설정",
+      navigate: "",
+    },
+  ];
+
   return (
     <AppLayout>
-      <View>
-        <Text>마이페이지</Text>
+      <View style={styles.container}>
+        <View style={styles.profileBox}>
+          <Avatar.Image
+            size={80}
+            source={{
+              uri: "https://reactnative.dev/img/tiny_logo.png",
+            }}
+          />
+          <View style={styles.profileContents}>
+            <Text variant="titleLarge" style={styles.nickname}>
+              레오
+            </Text>
+            <Text variant="bodyMedium" style={styles.profile}>
+              프로필 관리
+            </Text>
+          </View>
+        </View>
+        <List.Section style={styles.list}>
+          {menuList.map(({ title }) => {
+            return (
+              <List.Item
+                key={title}
+                title={title}
+                titleStyle={styles.listItem}
+                right={() => (
+                  <List.Icon color={theme.colors.white} icon="chevron-right" />
+                )}
+              />
+            );
+          })}
+        </List.Section>
+        <List.Section style={styles.list}>
+          {settings.map(({ title }) => {
+            return (
+              <List.Item
+                key={title}
+                title={title}
+                titleStyle={styles.listItem}
+                right={() => (
+                  <List.Icon color={theme.colors.white} icon="chevron-right" />
+                )}
+              />
+            );
+          })}
+        </List.Section>
       </View>
     </AppLayout>
   );
 }
 
 export default UserHome;
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+  profileBox: {
+    flexDirection: "row",
+    backgroundColor: theme.colors.gray900,
+    padding: 24,
+    borderRadius: 8,
+  },
+  profileContents: {
+    marginLeft: 32,
+  },
+  nickname: {
+    color: theme.colors.white,
+    marginBottom: 8,
+  },
+  profile: {
+    color: theme.colors.primary,
+  },
+  list: {
+    marginTop: 56,
+  },
+  listItem: {
+    color: theme.colors.white,
+  },
+});
